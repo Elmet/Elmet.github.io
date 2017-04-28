9715de465ddec4c5bf91698ca91bba8f833e080e
Anna-Karin Elmberg <annakarin.elmberg@gmail.com>
2017-04-28 23:36:47 +0200

    Assignment 16
    Solved assignment 16

--- script/scriptInl16.js
+++ script/scriptInl16.js
@@ -1,82 +1,110 @@
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.getData = this.getData.bind(this);
+        this.deleteListItem = this.deleteListItem.bind(this);
         this.state = {
             data: []
-        }
-        this.btnClickGetData = this.btnClickGetData.bind(this);
+        };
     }
     
+    render() {
+        return (
+            <div>
+            <h2 id='headline'>Country facts</h2>
+            
+            <CountryTable list={this.state.data} onDelete={this.deleteListItem} />
+            </div>
+        );
+    }
     
-    btnClickGetData(event) { 
+    componentDidMount() {
+        setTimeout(() => {
+            this.getData()
+        }, 3000
+        );
+    }
+    
+    getData() { 
         let self = this;
-        fetch('http://forverkliga.se/JavaScript/api/simple.php?world=whatever')
+        fetch('https://forverkliga.se/JavaScript/api/simple.php?world=whatever')
         .then(function(response) {
             return response.json()
         })
         .then(function(json) {
-            console.log('json', json);
+            //console.log('json', json);
             self.setState({
                 data: json
-            })
-             
-        })
+            });
+        });
     }
-
-    render() {
-        return (
-            <div>
-            <GetDataBtn clickEvent={ this.btnClickGetData } />
-            <GetDataMessage />
-            <ObjList data={this.state.data}></ObjList>
-            </div>
-        );
-    }
-}
-
-
-class GetDataBtn extends React.Component {
-render() {
-    return (
-        <button onClick = { this.props.clickEvent }>
-            Hämta data
-        </button>
-    );
-    }
-}
-
-class GetDataMessage extends React.Component {
-render() {
-    return (
-        <div>
-           Data hämtas...
-            
-        </div>
-    );
-    }
-}
-
     
-class ObjList extends React.Component {
-render() {
-    const list = this.props.data.map(
-        obj =><li key={obj.name}>{obj.name}{obj.continent}{obj.population}{obj.pFemale}</li>);
-    return (
-        <div>
-            <ul>
-                {list}
-            </ul>
-        </div>
-    );  
+    deleteListItem(event){
+        event.stopPropagation();
+        let tableRow = event.target.parentElement.parentElement;
+        
+        let list = this.state.data;
+        let newList = list.filter(x => x.name !== tableRow.id);
+        this.setState({
+            data: newList
+        });
+    } 
+}
+
+class CountryTable extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { count: 0 };
+    }
+    render() {
+        const tableRows = this.props.list.map(x =>
+        <TableRow key={x.name} item={x} clickEvent=
+        {this.props.onDelete} />
+        );
+        
+        if(tableRows.length === 0) {
+            return (
+            <div id='loadMessage'>Loading data...</div>)
+        }
+        else {
+        return (
+            <table className="countryFacts">
+            <thead>
+                <tr>
+                    <th>Country</th>
+                    <th>Continent</th>
+                    <th>Population</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableRows}
+                <tr key="sumRows" id="sum-row">
+                    <td colSpan="3">Sum number of countries: {tableRows.length}</td>
+                </tr>
+            </tbody>
+        </table>);
+        }
     }
 }
 
-class SumObj extends React.Component {
-render() {
-    return (
-        <p>sumobj</p>
-    );
+class TableRow extends React.Component {
+    constructor (props) {
+        super(props);  
+        
+        this.state = {item: this.props.item};
+    }
+    
+    render() {
+        const item = this.state.item;
+        let tableRow = (<tr key={item.name} id={item.name}>
+            <td>{item.name}</td>
+            <td>{item.continent}</td>
+            <td>{item.population}</td>
+            <td><button onClick={this.props.clickEvent}>Delete table row</button></td>
+            </tr>);
+        
+        return tableRow;
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('App'));
+ReactDOM.render(<App />, document.getElementById('app'));
